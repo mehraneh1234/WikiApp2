c3579f1d4ba1c2c7ca70220c15fad9b76c138f2a
mehraneh1234 <132540431+mehraneh1234@users.noreply.github.com>
2023-10-04 12:42:17 +0800

    Add comments
    Add comments

--- Information.cs
+++ Information.cs
@@ -6,31 +6,34 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Diagnostics;
 
 namespace WikiApplication
-{
+{// To save the data in a file it is eligible to write this code.
     [Serializable]
+    // Define the class which is inherited by IComparable. 
     public class Information : IComparable<Information>
     {
-        
+        // Construct the class
         public Information()
         {
         }
-
+        // Define all properties in private 
         private string _name;
         private string _category;
         private string _definition;
         private string _structure;
        
-      
+        // This method should be defined because of using IComparable. It compares the current name with the new one.  
         public int CompareTo(Information other)
         {
             string thisName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(this._name);
             string otherName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(other._name);
+            Trace.TraceInformation("Compare the name: {0}", thisName);
             return String.Compare(this._name, other._name, StringComparison.OrdinalIgnoreCase);
         }
 
-     
+        // Define getter methods of the properties.
         public string GetName()
         {
             return _name;
@@ -50,7 +53,7 @@ namespace WikiApplication
         {
             return _definition;
         }
-
+        // Define setter methods of the properties
         public void SetName(string name)
         {
             _name = name;
--- WikiForm.cs
+++ WikiForm.cs
@@ -11,34 +11,39 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Diagnostics;
 
-
+//Assessment Task two
+// Mehraneh Hamedani - 30062786 - 04/10/2023
 namespace WikiApplication
 {
     public partial class WikiForm : Form
     {
-       
+       // Create a list of (information) class
         List<Information> information = new List<Information>();
-        
+         
         public WikiForm()
-        {
-            InitializeComponent();
-            
+        {// Call the method
+            InitializeComponent();  
         }
-
+        #region Initialize the list view and combo box when form is loaded
+        // Make a method to display by default the title of the list view.
         private void InitializeListView()
         {
             listViewData.View = View.Details;
             listViewData.Columns.Add("Name");
             listViewData.Columns.Add("Category");
         }
-
+        // When form load load the data of combobox and call the method InitializeListView()
         private void WikiForm_Load(object sender, EventArgs e)
         {
             string filePath = "categories.dat";
             ReadCategories(filePath);
             InitializeListView();
         }
+        #endregion
+        #region Display list view and clear text boxes
+        // Clear the list view and if all properties are filled by the user. the data of them are showed in the list view.
         private void DisplayListViewData()
         {
             listViewData.Items.Clear();
@@ -60,7 +65,7 @@ namespace WikiApplication
             }
         }
 
-
+        // All of the input tools clear and the cursor will be on name textbox.
         private void ClearTextBox()
         {
             txtName.Clear();
@@ -70,7 +75,9 @@ namespace WikiApplication
             comboBoxCategory.Text = "";
             txtName.Focus();
         }
-        #region Category read, save, selected index change
+        #endregion
+        #region Load combobox data from a file
+        // Read the combobox from the file and check if there is any problem or the file does not exist.
         private void ReadCategories(string filePath)
         {
             try
@@ -89,31 +96,10 @@ namespace WikiApplication
                 MessageBox.Show($"Error reading categories: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-      
-
-   /*     private void comboBoxCategory_SelectedIndexChanged(object sender, EventArgs e)
-        { 
-            if (listViewData.SelectedItems.Count > 0)
-            {
-                Information selectedInformation = GetSelectedInformation();
-                selectedInformation.SetCategory(comboBoxCategory.SelectedItem.ToString());
-                DisplayListViewData();
-            } 
-        }
-
-        private Information GetSelectedInformation()
-        {
-            if (listViewData.SelectedItems.Count > 0)
-            {
-                ListViewItem selectedItem = listViewData.SelectedItems[0];
-                string selectedName = selectedItem.Text;
-                return information.FirstOrDefault(ds => ds.GetName() == selectedName);
-            }
-            return null;
-        }*/
+     
         #endregion
         #region radio buttons for structure
-
+        // In this method fill the text of the radio button in related to which radio button is checked.
         private string GetRadioButton()
         {
             string radioText = "";
@@ -123,7 +109,7 @@ namespace WikiApplication
                 radioText = radioButtonNonLinear.Text;
             return radioText;
         }
-
+        // It depends on the data which saved for the (structure) property the appropriate radio button is selected.
         private void SetRadioButton(int index)
         {
             if (information[index].GetStructure() == "Linear")
@@ -134,8 +120,12 @@ namespace WikiApplication
    
         #endregion
         #region Add
+        // Add data with a unique name to the list but before that check if all input properties are filled or not.
+        // Then Display the appropriate message.
         private void buttonAdd_Click(object sender, EventArgs e)
         {
+            //StreamWriter TextWriter;
+            //TextWriter = new StreamWriter("DebugCompare.txt", true);
             StatusStripDataStr.Items.Clear();
             string name = txtName.Text.Trim();
             string category = comboBoxCategory.Text.Trim();
@@ -148,28 +138,36 @@ namespace WikiApplication
                 StatusStripDataStr.Items.Add("Please fill in all fields.");
                 return;
             }
+            Trace.TraceInformation("Compare the name: {0} with existing data.", name);
+            //TextWriter.WriteLine("Compare the name: {0}", name);
 
             if (information.Any(info => info.GetName() == name))
             {
                 StatusStripDataStr.Items.Add("Duplicate Name. Please enter a unique name.");
+                Trace.TraceInformation("Duplicate Name");
+                //TextWriter.WriteLine("Duplicate Name");
             }
-            
-            Information newInformation = new Information();
-            newInformation.SetName(name);
-            newInformation.SetCategory(category);
-            newInformation.SetDefinition(definition);
-            newInformation.SetStructure(structure);
-            information.Add(newInformation);
-
-            ClearTextBox();
-            DisplayListViewData();
-            StatusStripDataStr.Items.Add("The new data structure is added.");
+            else
+            {
+                //TextWriter.Close();
+                Information newInformation = new Information();
+                newInformation.SetName(name);
+                newInformation.SetCategory(category);
+                newInformation.SetDefinition(definition);
+                newInformation.SetStructure(structure);
+                information.Add(newInformation);
+                information.Sort();
+                ClearTextBox();
+                DisplayListViewData();
+                StatusStripDataStr.Items.Add("The new data structure is added.");
+            }            
 
         }
 
      
         #endregion
         #region Load
+        // By click on this button the data of binary file loaded and display in the list view.
         private void buttonLoad_Click(object sender, EventArgs e)
         {
             StatusStripDataStr.Items.Clear();
@@ -211,6 +209,8 @@ namespace WikiApplication
 
         #endregion
         #region Save button and close form to save
+        // By click on this button all data in the list will be saved in the binary file which is called definitions.
+        //By using try and catch control if there is any problem.
         private void buttonSave_Click(object sender, EventArgs e)
         {
             StatusStripDataStr.Items.Clear();
@@ -254,7 +254,8 @@ namespace WikiApplication
 
         #endregion
         #region Edit
-
+        // By click oe Edit button check if one item is selected in the list view and all the input properties are filled then 
+        // allow to edit that item.
         private void buttonEdit_Click(object sender, EventArgs e)
         {
             StatusStripDataStr.Items.Clear();
@@ -274,18 +275,20 @@ namespace WikiApplication
                     information[selectedRowIndex].SetCategory(category);
                     information[selectedRowIndex].SetStructure(structure);
                     information[selectedRowIndex].SetDefinition(definition);
+                    StatusStripDataStr.Items.Add("The selected row is edited.");
                 }
             }
             else
             {
                 StatusStripDataStr.Items.Add("Please select a row to edit.");
             }
-            StatusStripDataStr.Items.Add("The selected row is edited.");
+            
             ClearTextBox();
             DisplayListViewData();
         }
         #endregion
         #region Delete and double click on text box name and list view
+        // By click on delete button pop up a window to ask about deleting and if select an item in the ist view, it allows to delete.
         private void buttonDelete_Click(object sender, EventArgs e)
         {
             StatusStripDataStr.Items.Clear();
@@ -298,6 +301,7 @@ namespace WikiApplication
                     int selectedIndex = listViewData.SelectedItems[0].Index;
                     information.RemoveAt(selectedIndex);
                     DisplayListViewData();
+                    StatusStripDataStr.Items.Add("The selected item is deleted.");
                 }
             }
             else
@@ -322,6 +326,8 @@ namespace WikiApplication
 
         #endregion
         #region Search
+        // First sort the file then check if the search text box is filled then browsing the item if the name
+        // of that item exists in the file then fill the input properties with the appropriate data of that item.
         private void buttonSearch_Click(object sender, EventArgs e)
         {
             ClearTextBox();
@@ -377,11 +383,12 @@ namespace WikiApplication
         }
         #endregion
         #region Capitalize initial name and double click on txtName to clear all
+        // To capitalize the initial of the name.
         private void txtName_Leave(object sender, EventArgs e)
         {
             txtName.Text = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(txtName.Text);
         }
-
+        // By double click on the name textbox all input properties will be cleared.
         private void txtName_DoubleClick_1(object sender, EventArgs e)
         {
             ClearTextBox();
@@ -390,7 +397,8 @@ namespace WikiApplication
 
 
         #endregion
-
+        #region Select index change in the list view
+        // By select an item in the list view all data of that item display in the input properties.
         private void listViewData_SelectedIndexChanged(object sender, EventArgs e)
         {
             StatusStripDataStr.Items.Clear();
@@ -405,5 +413,6 @@ namespace WikiApplication
                 txtDefinition.Text = information[index].GetDefinition();
             }
         }
+        #endregion
     }
 }
